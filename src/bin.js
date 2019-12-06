5673d4f0148f8b0caacd98c9d3cc08ed6e7dfbb1
Brandon Farmer <bthesorceror@gmail.com>
2019-12-06 13:28:24 -0800

    Make ipc a flag for binary

--- src/bin.js
+++ src/bin.js
@@ -15,6 +15,11 @@ const argv = yargs
     type: 'string',
     describe: 'debug log file path'
   })
+  .option('ipc', {
+    type: 'boolean',
+    describe: 'use ipc for transport',
+    default: false
+  })
   .argv
 
 let log = () => { }
@@ -30,16 +35,42 @@ if (logfile && logfile !== '') {
 
 const server = new Server(argv['disable-folding'])
 
-process.stdin.on('data', (data) => {
-  const input = data.toString()
+const respond = (data) => {
+  if (argv.ipc) {
+    const lines = data.split('\n')
+    if (lines.length === 3) {
+      process.send(JSON.parse(lines[2]))
+    }
+  } else {
+    process.stdout.write(data)
+  }
+}
 
-  log(`REQUEST: ${input}`)
+const handleInput = (data) => {
+  let input = data
+
+  if (argv.ipc) {
+    input = `\n\n${JSON.stringify(input)}`
+  }
+
+  log(`REQUEST: ${input}\n`)
 
   const resp = server.process(input)
-  const msg = resp.get_message()
 
+  const msg = resp.get_message()
   if (msg) {
-    log(`RESPONSE: ${msg}`)
-    process.stdout.write(msg)
+    log(`RESPONSE: ${msg}\n`)
+    respond(msg)
   }
-})
+
+  const err = resp.get_error()
+  if (err) {
+    log(`ERROR: ${err}\n`)
+  }
+}
+
+if (argv.ipc) {
+  process.on('message', handleInput)
+} else {
+  process.stdin.on('data', handleInput)
+}
