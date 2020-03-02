a46a0a13da1d16e19a030e8a142b48e4a4193a50
Brandon Farmer <bthesorceror@gmail.com>
2020-03-02 15:10:50 -0800

    Clean up unneeded build steps

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-npm run bundle
-
-cp node_modules/@influxdata/flux-lsp-node/flux-lsp-node_bg.wasm out/
-cp node_modules/@influxdata/flux-lsp-node/flux-lsp-node_bg.js out/
-cp node_modules/@influxdata/flux-lsp-node/flux-lsp-node.js out/
